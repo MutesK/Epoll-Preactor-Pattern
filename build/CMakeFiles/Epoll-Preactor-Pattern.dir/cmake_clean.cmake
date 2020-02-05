f44220669f23b7009ca981c96abb3dcdf95b3abf
Mutesk <j0803@naver.com>
2020-02-05 17:41:56 +0900

    Added CMake

---
+++ build/CMakeFiles/Epoll-Preactor-Pattern.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Epoll-Preactor-Pattern.dir/main.cpp.o"
+  "Epoll-Preactor-Pattern.pdb"
+  "Epoll-Preactor-Pattern"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Epoll-Preactor-Pattern.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mute/Documents/Workspace/Epoll-Preactor-Pattern
+# Build directory: /home/mute/Documents/Workspace/Epoll-Preactor-Pattern/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
